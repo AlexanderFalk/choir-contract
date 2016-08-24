7371d2c6c1bc1770fd4c58d6101c6b1d5ce6b5de
Anders Kalhauge <anders@kalhauge.dk>
2016-08-24 12:27:24 +0200

    PlantUML added to package-info

--- src/main/java/dk/cphbusiness/choir/contract/package-info.java
+++ src/main/java/dk/cphbusiness/choir/contract/package-info.java
@@ -1,6 +1,14 @@
 /**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Demonstration for PlantUML.
+ * <p>
+ * Example of use:
+ * <p>
+ * <img src="contract.png">
+ */
+/*
+ * @startuml contract.png
+ * Bob -> Alice : hello
+ * Alice --> Bob : OK
+ * @enduml
  */
 package dk.cphbusiness.choir.contract;
