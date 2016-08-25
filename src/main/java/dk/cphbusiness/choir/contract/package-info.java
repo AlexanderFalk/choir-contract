1e6b418f4cd4b7e3f69bc4cee21d684e51fe8530
Anders Kalhauge <anders@kalhauge.dk>
2016-08-25 10:12:15 +0200

    Logical data model diagram

--- src/main/java/dk/cphbusiness/choir/contract/package-info.java
+++ src/main/java/dk/cphbusiness/choir/contract/package-info.java
@@ -1,13 +1,51 @@
 /**
- * Demonstration for PlantUML.
+ * Toolbox contract for Choir System
  * <p>
  * Example of use:
  * <p>
  * <img src="contract.png">
  *
  * @startuml contract.png
- * Bob -> Alice : hello
- * Alice --> Bob : OK
+ * abstract class Person  {
+ *   firstName
+ *   lastName
+ *   dayOfBirth
+ *   }
+ * class Member {
+ *   phone
+ *   email
+ *   }
+ * class Artist {
+ *   dayOfDeath[0..1]
+ *   wiki: Url
+ *   }
+ * class Voice {
+ *   name
+ *   }
+ * class Music {
+ *   title
+ *   opus
+ *   }
+ * class Material {
+ *   title
+ *   file
+ *   }
+ * class Audio {
+ *   playingTime
+ *   }
+ * class Sheet {
+ *   pageCount 
+ *   }
+ * Person <|-- Member
+ * Person <|-- Artist
+ * Member "*" -- "1" Voice
+ * Music "*" -- "1" Artist : composer
+ * Music "*" -- "0..1" Artist : poet
+ * Material <|-- Audio
+ * Material <|-- Sheet
+ * Material "*" -- "1" Music
+ * Material "*" -- "1..*" Voice
+ * hide circle
  * @enduml
  */
 package dk.cphbusiness.choir.contract;
