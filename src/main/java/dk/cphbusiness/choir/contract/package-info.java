c8a8ef7790c810655b5c0d9d43603771e0af5175
Anders Kalhauge <anders@kalhauge.dk>
2016-08-25 10:54:18 +0200

    Use case diagram

--- src/main/java/dk/cphbusiness/choir/contract/package-info.java
+++ src/main/java/dk/cphbusiness/choir/contract/package-info.java
@@ -1,8 +1,13 @@
 /**
  * Toolbox contract for Choir System
  * <p>Logical Data Model</p>
+ * 
+ * <img src="ldm.png"/>
  *
- * @startuml contract.png
+ * <p>Use case diagram</p>
+ * <img src="ucd.png"/>
+ * 
+* @startuml ldm.png
  * abstract class Person  {
  *   firstName
  *   lastName
@@ -33,16 +38,40 @@
  * class Sheet {
  *   pageCount 
  *   }
+ * class Repertoire {
+ *   name
+ *   description
+ *   }
  * Person <|-- Member
  * Person <|-- Artist
  * Member "*" -- "1" Voice
  * Music "*" -- "1" Artist : composer
  * Music "*" -- "0..1" Artist : poet
+ * Music "*" -- "*" Repertoire
  * Material <|-- Audio
  * Material <|-- Sheet
  * Material "*" -- "1" Music
  * Material "*" -- "1..*" Voice
  * hide circle
+ * @enduml
+ * 
+  * @startuml ucd.png
+ * (Find Material for Member) as (UC1)
+ * (Print birthday list) as (UC2)
+ * (List music in repertoire) as (UC3)
+ * (Edit Member) as (UC4)
+ * (CRUD Material) as (UC5)
+ * (Login) as (UC6)
+ * actor Member
+ * actor Administrator
+ * Member --> (UC1)
+ * Member --> (UC2)
+ * Member --> (UC3)
+ * Member --> (UC4)
+ * Member --> (UC6)
+ * (UC4) <-- Administrator
+ * (UC5) <-- Administrator
+ * (UC6) <-- Administrator
  * @enduml
  */
 package dk.cphbusiness.choir.contract;
